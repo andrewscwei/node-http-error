648371c8188cf6b032ee99151fb05adf05587505
Andrew Wei <andrewscwei@gmail.com>
2018-10-30 10:59:08 -0700

    Update tsconfig.json

--- build/index.js
+++ build/index.js
@@ -1,4 +1,5 @@
 "use strict";
+/* tslint:disable max-classes-per-file */
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
